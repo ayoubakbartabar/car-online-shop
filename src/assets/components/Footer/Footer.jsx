f22d4fa3deca2047a7ff4b3ebc5d0e54865af199
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-03-24 14:20:33 +0330

    edite footer

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Footer.css'
-import Information from './Information'
+import Owners from './Owners'
 import MyAccount from './MyAccount'
 import StoreInfo from './StoreInfo'
 export default function Footer() {
   return (
     <section className='footer-container'>
-      <Information/>
+      <Owners/>
       <MyAccount/>
       <StoreInfo/>
     </section>
---
+++ src/assets/components/Footer/Owners.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Owners() {
+  return (
+    <div className='owners-container'>
+        <h2 className="owners-title">owners</h2>
+        <ul className="owners-list-container">
+        <li className="owners-list">carYard</li>
+        </ul>
+    </div>
+  )
+}
