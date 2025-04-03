00150693be3982021130df93ade3386f25863646
Ayoub <162346549+ayoubakbartabar@users.noreply.github.com>
2025-04-03 21:04:33 +0330

    Update OtherCompanyData.js

--- src/assets/components/OtherCompany/OtherCompanyData.js
+++ src/assets/components/OtherCompany/OtherCompanyData.js
@@ -1,11 +1,11 @@
 const otherCompanyData = [
-    {id:1,company:"Maserati" , href:"https://www.maserati.com/us/en" , image:'public/image/other-company-image1.png'},
-    {id:3,company:"Aston Martin" , href:"https://www.astonmartin.com/en" , image:'public/image/other-company-image3.png'},
-    {id:4,company:"Tesla " , href:"https://www.tesla.com/" , image:'public/image/other-company-image4.png'},
-    {id:5,company:"Jaguar " , href:"https://www.jaguar.com/market-selector.html" , image:'public/image/other-company-image5.png'},
-    {id:6,company:"Chevrolet" , href:"https://www.chevrolet.com/" , image:'public/image/other-company-image6.png'},
-    {id:8,company:"Bentley" , href:"https://www.bentleymotors.com/en.html" , image:'public/image/other-company-image8.png'},
-    {id:9,company:"Lexus" , href:"https://www.lexus.com/" , image:'public/image/other-company-image9.png'},
-    {id:10,company:"Mercedes-Benz" , href:"https://www.mercedes-benz.com/en/" , image:'public/image/other-company-image10.png'},
+    {id:1,company:"Maserati" , href:"https://www.maserati.com/us/en" , image:'/image/other-company-image1.png'},
+    {id:3,company:"Aston Martin" , href:"https://www.astonmartin.com/en" , image:'/image/other-company-image3.png'},
+    {id:4,company:"Tesla " , href:"https://www.tesla.com/" , image:'/image/other-company-image4.png'},
+    {id:5,company:"Jaguar " , href:"https://www.jaguar.com/market-selector.html" , image:'/image/other-company-image5.png'},
+    {id:6,company:"Chevrolet" , href:"https://www.chevrolet.com/" , image:'/image/other-company-image6.png'},
+    {id:8,company:"Bentley" , href:"https://www.bentleymotors.com/en.html" , image:'/image/other-company-image8.png'},
+    {id:9,company:"Lexus" , href:"https://www.lexus.com/" , image:'/image/other-company-image9.png'},
+    {id:10,company:"Mercedes-Benz" , href:"https://www.mercedes-benz.com/en/" , image:'/image/other-company-image10.png'},
 ]
 export default otherCompanyData
