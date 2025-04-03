6259c86058426f247a4ce041cbc5ea5ac18a51ab
Ayoub <162346549+ayoubakbartabar@users.noreply.github.com>
2025-04-03 21:01:53 +0330

    Update Discount.jsx

--- src/assets/components/Discount/Discount.jsx
+++ src/assets/components/Discount/Discount.jsx
@@ -7,8 +7,8 @@ export default function Discount() {
             <h3 className="discount-title">new mercedes</h3>
             <p className="discount-paragraph">you can save up 20 %</p>
         </div>
-        <img className="discount-image" src="public/image/discount-image.png" alt="new car"/>
-        <img className="discount-image-max-width-750" src="public/image/discount-image.png" alt="new car"/>
+        <img className="discount-image" src="/image/discount-image.png" alt="new car"/>
+        <img className="discount-image-max-width-750" src="/image/discount-image.png" alt="new car"/>
     </section>
   )
 }
